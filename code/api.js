470e2c14ac20de9153eeda29dfa1a9a02efe9e67
pazlort <pazmarialort@gmail.com>
2023-10-20 19:06:15 -0300

    api funcionando

--- code/api.js
+++ code/api.js
@@ -1,16 +1,42 @@
-var contenido = document.querySelector('#contenido');
+const { createApp } = Vue 
+createApp({
+    data(){
+        return{
+            nombreDeBusqueda: '',
+            arrayNombre:[],
+            url:'',
+            libro:{},
+            librosEncontrados:[],
+        }
+    },
+    methods:{
+        buscarLibros() {
+            librosEncontrados=[]
+            arrayNombre=this.nombreDeBusqueda.split(' ')
+            for (let i = 0; i < arrayNombre.length; i++) {
+                i<(arrayNombre.length-1)?this.url+=arrayNombre[i]+'+':this.url+=arrayNombre[i];
+            }
+            this.url="https://openlibrary.org/search.json?q="+this.url
+            fetch(this.url)
+                .then(res => res.json())
+                .then(datos => {
+                    for (let i = 0; i < 21; i++) {
+                        const libros = datos.docs[i];
+                        if (libros.cover_i !== undefined) {
+                            this.libro={
+                                nombre: libros.title,
+                                autor:libros.author_name[0],
+                                anioPrimerPublicacion: libros.first_publish_year,
+                                isbn: libros.isbn[0],
+                                editorial: libros.publisher[0],
+                                imagen: "https://covers.openlibrary.org/b/id/"+libros.cover_i+"-L.jpg",
+                            }
+                            this.librosEncontrados.push(this.libro)
+                        }
+                        console.log(this.librosEncontrados);
 
-function traer() {
-    fetch('https://openlibrary.org/search.json?author=tolkien&sort=new')
-
-        .then(res => res.json())
-        .then(data => {
-            console.log(data.docs[0])
-            //console.log(data.results)
-            // console.log(data.results[0].email)
-            // contenido.innerHTML =
-            // `<img src="${data.results[0].picture.large}" width="100px" class="img-fluid rounded-circle">
-            // <p>Nombre: ${data.results[0].name.first}</p>
-            // `
-        })
-}
+                    }
+                })
+        },
+    },
+}).mount('#app')
