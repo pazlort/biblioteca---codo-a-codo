ee2dd3bef7ed70ee622b947a1c348df4e741ff86
pazlort <pazmarialort@gmail.com>
2023-11-28 15:50:44 -0300

    agrego db y borro img viejas

--- python/modelo.py
+++ python/modelo.py
@@ -8,7 +8,7 @@ from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 
 # Instalar con pip install mysql-connector-python
-#import mysql.connector
+import mysql.connector
 
 # Si es necesario, pip install Werkzeug
 from werkzeug.utils import secure_filename
