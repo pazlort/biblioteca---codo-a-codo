b91a6be0b27f8f6d42593e0bc4e6cce19064eca1
pazlort <pazmarialort@gmail.com>
2023-11-28 15:53:28 -0300

    agrego db y borro img viejas

--- sql/Bookbuster.sql
+++ sql/Bookbuster.sql
@@ -2,14 +2,13 @@ CREATE DATABASE IF NOT EXISTS Bookbuster CHARACTER SET UTF8MB4 COLLATE UTF8MB4_U
 
 -- Crear la tabla de usuarios
 CREATE TABLE IF NOT EXISTS usuarios (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	id_user INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	username VARCHAR(50) NOT NULL,
-    password VARCHAR(100) NOT NULL,
+    pass_user VARCHAR(100) NOT NULL,
 	firstname VARCHAR(50) NOT NULL,
 	lastname VARCHAR(50) NOT NULL,
-	birthdate DATE,
-    rol_id TINYINT(1),
+    id_rol TINYINT(1),
 	create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	update_at TIMESTAMP NULL,
 	delete_at TIMESTAMP NULL,
@@ -19,7 +18,7 @@ CREATE TABLE IF NOT EXISTS usuarios (
 
 -- Crear la tabla de roles
 CREATE TABLE IF NOT EXISTS roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_rol INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     update_at TIMESTAMP NULL,
@@ -30,12 +29,11 @@ CREATE TABLE IF NOT EXISTS roles (
 
 -- Crear la tabla de libros
 CREATE TABLE IF NOT EXISTS libros (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    ISBN VARCHAR(255) NOT NULL UNIQUE,
-    titulo_libto VARCHAR(50) NOT NULL,
+    id_libro INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    titulo_libro VARCHAR(50) NOT NULL,
     autor_libro VARCHAR(100) NOT NULL,
     editorial_libro VARCHAR(50) NOT NULL,
-    fecha_publiacion DATE,
+    url_img VARCHAR(100) NOT NULL,
     create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     update_at TIMESTAMP NULL,
     delete_at TIMESTAMP NULL,
@@ -45,9 +43,9 @@ CREATE TABLE IF NOT EXISTS libros (
 
 -- Crear la tabla de prestamos
 CREATE TABLE IF NOT EXISTS prestamos (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    usuario_id INT NOT NULL,
-    libro_id INT NOT NULL,
+    id_prestamo INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_user INT NOT NULL,
+    id_libro INT NOT NULL,
     fecha_desde DATETIME NOT NULL,
     fecha_hasta INT NOT NULL,
     observaciones VARCHAR(255),
@@ -141,13 +139,13 @@ END;
 DELIMITER ;
 
 INSERT INTO usuarios
-    (email, username, PASSWORD, firstname, lastname, birthdate, rol_id, create_at, update_at, delete_at, delete_by_user, state)
+    (email, username, pass_user, firstname, lastname, id_rol, create_at, update_at, delete_at, delete_by_user, state)
     VALUES
-    ('usuario1@example.com', 'usuario1', 'clave1', 'Nombre1', 'Apellido1', '1990-01-01', '1', NOW(), NULL, NULL, NULL, 1),
-    ('usuario2@example.com', 'usuario2', 'clave2', 'Nombre2', 'Apellido2', '1995-03-15', '2', NOW(), NULL, NULL, NULL, 1),
-    ('usuario3@example.com', 'usuario3', 'clave3', 'Nombre3', 'Apellido3', '1988-07-22', '2', NOW(), NULL, NULL, NULL, 1),
-    ('usuario4@example.com', 'usuario4', 'clave4', 'Nombre4', 'Apellido4', '2000-12-05', '3', NOW(), NULL, NULL, NULL, 1),
-    ('usuario5@example.com', 'usuario5', 'clave5', 'Nombre5', 'Apellido5', '1998-09-10', '3', NOW(), NULL, NULL, NULL, 1);
+    ('usuario1@example.com', 'usuario1', 'clave1', 'Nombre1', 'Apellido1', '1', NOW(), NULL, NULL, NULL, 1),
+    ('usuario2@example.com', 'usuario2', 'clave2', 'Nombre2', 'Apellido2', '2', NOW(), NULL, NULL, NULL, 1),
+    ('usuario3@example.com', 'usuario3', 'clave3', 'Nombre3', 'Apellido3', '2', NOW(), NULL, NULL, NULL, 1),
+    ('usuario4@example.com', 'usuario4', 'clave4', 'Nombre4', 'Apellido4', '3', NOW(), NULL, NULL, NULL, 1),
+    ('usuario5@example.com', 'usuario5', 'clave5', 'Nombre5', 'Apellido5', '3', NOW(), NULL, NULL, NULL, 1);
 
 INSERT INTO roles
     (name, create_at, update_at, delete_at, delete_by_user, state)
@@ -157,16 +155,16 @@ INSERT INTO roles
     ('Usuario', NOW(), NULL, NULL, NULL, 1);
 
 INSERT INTO libros
-    (ISBN, titulo_libto, autor_libro, editorial_libro, fecha_publiacion, create_at, update_at, delete_at, delete_by_user, state)
+    (titulo_libro, autor_libro, editorial_libro, url_img, create_at, update_at, delete_at, delete_by_user, state)
     VALUES
-    ('ISBN1', 'titulo1', 'autor1', 'editorial1', '2020-01-01', NOW(), NULL, NULL, NULL, 1),
-    ('ISBN2', 'titulo2', 'autor2', 'editorial2', '2020-01-01', NOW(), NULL, NULL, NULL, 1),
-    ('ISBN3', 'titulo3', 'autor3', 'editorial3', '2020-01-01', NOW(), NULL, NULL, NULL, 1),
-    ('ISBN4', 'titulo4', 'autor4', 'editorial4', '2020-01-01', NOW(), NULL, NULL, NULL, 1),
-    ('ISBN5', 'titulo5', 'autor5', 'editorial5', '2020-01-01', NOW(), NULL, NULL, NULL, 1);
+    ('titulo1', 'autor1', 'editorial1', '2020-01-01', NOW(), NULL, NULL, NULL, 1),
+    ('titulo2', 'autor2', 'editorial2', '2020-01-01', NOW(), NULL, NULL, NULL, 1),
+    ('titulo3', 'autor3', 'editorial3', '2020-01-01', NOW(), NULL, NULL, NULL, 1),
+    ('titulo4', 'autor4', 'editorial4', '2020-01-01', NOW(), NULL, NULL, NULL, 1),
+    ('titulo5', 'autor5', 'editorial5', '2020-01-01', NOW(), NULL, NULL, NULL, 1);
 
 INSERT INTO prestamos
-    (usuario_id, libro_id, fecha_desde, fecha_hasta, observaciones, create_at, update_at, delete_at, delete_by_user, state)
+    (id_user, id_libro, fecha_desde, fecha_hasta, observaciones, create_at, update_at, delete_at, delete_by_user, state)
     VALUES
     ('1', '1', '2020-01-01', '2020-12-31', 'observaciones', NOW(), NULL, NULL, NULL, 1),
     ('2', '2', '2020-01-01', '2020-12-31', 'observaciones', NOW(), NULL, NULL, NULL, 1),
